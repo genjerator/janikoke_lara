98bf26db572377ab905734ed64836568b2e44d07
genjerator <e.medjesi@gmail.com>
2025-09-27 12:18:48 +0200

    change radio url

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -7,7 +7,7 @@ function Welcome() {
         <>
             <div
                 className="relative sm:flex sm:justify-center sm:items-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                Андя пиє пивко...--
+                ...Андя пиє пивко...
             </div>
             <div
                 className="relative sm:flex sm:justify-center sm:items-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
